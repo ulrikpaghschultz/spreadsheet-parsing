0c3fd7c7b9006cdc3697d4402abb0803c285fd0d
ulrikpaghschultz <ups@mmmi.sdu.dk>
2015-06-07 13:38:24 +0200

    initial sample of generated code

---
+++ python/generated/example.py
@@ -0,0 +1,24 @@
+'''
+Created on Jun 7, 2015
+
+@author: ups
+'''
+
+print 1
+
+class ExampleParser(object):
+    '''
+    example of code that would be generated
+    '''
+
+    def __init__(self, spreadsheet):
+        '''
+        Constructor
+        '''
+        self.spreadsheet = spreadsheet
+
+    def matchColumns(self,columnHeaders):
+        return True # if the headers provided as a list of strings matches the columns of this parser object
+    
+    def parseBlock(self,columnHeaders,row,column,width,height):
+        return None # column headers not included in block, returns list of objects extracted from spreadsheet, or None if parse err
