8369fb4d294aa83886c28ffe99c935780a179a19
cisoa <sorin.adam@conpleks.com>
2015-06-08 11:39:07 +0200

    basic_test file uses relative path to "generated" directory

--- python/library/basic_test.py
+++ python/library/basic_test.py
@@ -1,9 +1,11 @@
+import sys
+sys.path.insert(0,'../generated')
 import SpreadParse
 import Parser_Questionnaire
 s=SpreadParse.Spreadsheet()
-s.InitFromFile("TestCsvFile.csv")
+s.InitFromFile("testCsvFile.csv")
 pq=Parser_Questionnaire.ParseQuestionnaire(s)
 headers=['Form','Name','Question','Type','Value','Condition']
 pq.matchColumns(headers)
 x=pq.parseBlock(headers,14,2,7)
-x
+print x
